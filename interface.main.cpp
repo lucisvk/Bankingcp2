cee336f2b7a0033e9c467deac459fd701f394ca5
Tyler Morency <110355447+lucisvk@users.noreply.github.com>
2024-06-13 13:23:11 -0400

    Update interface.main.cpp

--- interface.main.cpp
+++ interface.main.cpp
@@ -3,14 +3,26 @@
 #include <string>
 #include <vector>
 
+#include "acc_server.cpp"
+
 using namespace std; 
 
 
 int main () {
 
     cout << "Hello Welcome ApBanking Please chosse one of the following options." << endl; 
+    cout << "Deposit funds: 1" << endl;
+    cout << "Withdraw funds: 2" << endl;
+    cout << "check balance: 3" << endl; 
+    cout << "Create account: 4" << endl; 
     
+    int usrintput; 
+    cin >> usrintput; 
 
+    if ( usrintput == 1) 
+    {
+        depositfunds(usrintput);
+    }
 
 
     return 0;
